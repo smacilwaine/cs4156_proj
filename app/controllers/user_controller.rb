2322a033a2be4f07007bf0100a6f2842cb9e2cc9
OsamaKhurshid <ok2291@columbia.edu>
2020-03-05 19:40:19 -0500

    email added to db, email+role added to signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:username, :password))
+    @user = User.create(params.require(:user).permit(:username, :email, :password, :role))
     session[:user_id] = @user.id
     redirect_to '/welcome'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_000323) do
+ActiveRecord::Schema.define(version: 2020_03_06_003800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_000323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "email"
   end
 
 end

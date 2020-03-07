5cea88724a9ed0e45b461096be73e4bf3214eb04
rohit-lunavara <rohit.lunavara@gmail.com>
2020-03-06 20:49:57 -0500

    Validation for user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,11 +6,23 @@ class UserController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.create(params.require(:user).permit(:username, :email, :password, :role))
+  def validate
+    if @user.username.length < 5 || @user.password.length < 5 || URI::MailTo::EMAIL_REGEXP.match(@user.email) || @user.email.length < 1 || !@user.role
+      return false
+    else
+      return true
+    end
+  end
 
-    session[:user_id] = @user.id
-    redirect_to dashboard_path
+  def create
+    @user = User.new(params.require(:user).permit(:username, :email, :password, :role))
+    if @user.validate
+      redirect_to new_user_path
+    else
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    end
   end
 
   def dashboard
